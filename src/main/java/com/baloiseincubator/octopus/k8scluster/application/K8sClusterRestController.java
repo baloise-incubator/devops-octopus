97d5ded05da1641d4ce5ca0c86c9c7418e658f6d
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2020-05-02 16:43:30 +0200

    Add more endpoints

--- src/main/java/com/baloiseincubator/octopus/k8scluster/application/K8sClusterRestController.java
+++ src/main/java/com/baloiseincubator/octopus/k8scluster/application/K8sClusterRestController.java
@@ -1,7 +1,10 @@
 package com.baloiseincubator.octopus.k8scluster.application;
 
+import io.fabric8.kubernetes.api.model.KubernetesResource;
+import io.fabric8.kubernetes.api.model.LimitRangeList;
 import io.fabric8.kubernetes.api.model.NamespaceList;
 import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.openshift.api.model.ProjectList;
 import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftClient;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +18,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class K8sClusterRestController {
 
   @GetMapping("ns")
-  public NamespaceList getStuff() {
+  public NamespaceList getNamespaces() {
     OpenShiftClient osClient = new DefaultOpenShiftClient();
     return osClient.namespaces().withLabel("client", "joachim").list();
   }
 
   @GetMapping("pods")
-  public PodList getStuff2() {
+  public PodList getPods() {
     OpenShiftClient osClient = new DefaultOpenShiftClient();
     return osClient.pods().inNamespace("dialogflow-api").list();
   }
 
+  @GetMapping("quota")
+  public KubernetesResource getQuota() {
+    OpenShiftClient osClient = new DefaultOpenShiftClient();
+    return osClient.resourceQuotas().inNamespace("dialogflow-api").list();
+  }
+
+  @GetMapping("limits")
+  public LimitRangeList getLimitRanges() {
+    OpenShiftClient osClient = new DefaultOpenShiftClient();
+    return osClient.limitRanges().inNamespace("dialogflow-api").list();
+  }
+
+  @GetMapping("projects")
+  public ProjectList getProjects() {
+    OpenShiftClient osClient = new DefaultOpenShiftClient();
+    return osClient.projects().withLabel("client", "joachim").list();
+  }
+
+  @GetMapping("metrics")
+  public KubernetesResource getOther() {
+    OpenShiftClient osClient = new DefaultOpenShiftClient();
+    return osClient.top().pods().metrics("dialogflow-api");
+  }
 
 }
