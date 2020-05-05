009a07244f691e4b38fea3bef8e91bd46ecdaf96
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2020-05-05 22:03:45 +0200

    Add Rest API

--- src/main/java/dev/baloise/octopus/rollout/infrastructure/RolloutStatusRepository.java
+++ src/main/java/dev/baloise/octopus/rollout/infrastructure/RolloutStatusRepository.java
@@ -1,9 +1,15 @@
 package dev.baloise.octopus.rollout.infrastructure;
 
 import dev.baloise.octopus.rollout.domain.RolloutStatus;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RolloutStatusRepository extends CrudRepository<RolloutStatus, Integer> {
+
+  @Query("Select * from ROLLOUT_STATUS order by artifact_id desc")
+  List<RolloutStatus> findByOrOrderByArtifact_idAsc();
 }
